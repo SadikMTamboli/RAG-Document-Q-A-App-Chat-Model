f31ebde826ad13778633f9ebc50c1a3f05aed9db
SadikMTamboli <84015502+SadikMTamboli@users.noreply.github.com>
2025-12-02 21:48:47 +0530

    RAG_doc_QA.py

--- RAG_doc_QA.py
+++ RAG_doc_QA.py
@@ -1,17 +1,16 @@
 import os
-# import vectorstore
 import streamlit as st
+from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import OllamaEmbeddings
-from langchain_huggingface  import HuggingFaceEmbeddings
-from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_huggingface import HuggingFaceEmbeddings		
 from langchain_community.document_loaders import PyPDFLoader
-from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+
 load_dotenv()
 
 # os.environ['GROQ_API_KEY']=os.getenv('GROQ_API_KEY')
@@ -247,4 +246,5 @@ if opt=='PDF':
             #         st.write('--------------')
 elif opt=='WEB':
      st.write('Coming soon')
+
 
